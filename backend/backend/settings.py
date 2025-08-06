6682f6f4be13d8b018549bf388dbefaf8d977482
Neil <nj6604053@gmail.com>
2025-08-06 21:54:19 +0530

    updated req

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,9 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
     
     'corsheaders',
     "fns",
